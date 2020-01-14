2e664f02e7f8f99132062f29dc2d044695a127b5
Ankit Mittal <mitankit@amazon.com>
2020-01-14 15:13:39 -0800

    Add initial Audio Video Facade

--- amazon-chime-sdk/src/main/java/com/amazon/chime/sdk/session/DefaultMeetingSession.kt
+++ amazon-chime-sdk/src/main/java/com/amazon/chime/sdk/session/DefaultMeetingSession.kt
@@ -2,7 +2,7 @@ package com.amazon.chime.sdk.session
 
 import com.amazon.chime.sdk.utils.logger.Logger
 
-class DefaultMeetingSession(override val configuration: MeetingSessionConfiguration,
-                            override val logger: Logger): MeetingSession {
-
-}
+class DefaultMeetingSession(
+    override val configuration: MeetingSessionConfiguration,
+    override val logger: Logger
+) : MeetingSession
