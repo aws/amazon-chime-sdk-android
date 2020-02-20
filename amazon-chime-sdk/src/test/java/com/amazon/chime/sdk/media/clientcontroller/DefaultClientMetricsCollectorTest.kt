835b1df931e0a61d396828c47e9f25785813d37b
Henry Smith <hensmi@amazon.com>
2020-02-20 14:02:11 -0800

    Fix merge

--- amazon-chime-sdk/src/test/java/com/amazon/chime/sdk/media/clientcontroller/DefaultClientMetricsCollectorTest.kt
+++ amazon-chime-sdk/src/test/java/com/amazon/chime/sdk/media/clientcontroller/DefaultClientMetricsCollectorTest.kt
@@ -34,7 +34,7 @@ class DefaultClientMetricsCollectorTest {
 
     @Test
     fun `onMetrics should call observer after interval has passed`() {
-        Thread.sleep(1000)
+        Thread.sleep(1100)
 
         clientMetricsCollector.addObserver(mockAudioVideoObserver)
         val rawMetrics = mutableMapOf(AudioClient.AUDIO_CLIENT_METRIC_POST_JB_SPK_1S_PACKETS_LOST_PERCENT to 1.0)
@@ -46,7 +46,7 @@ class DefaultClientMetricsCollectorTest {
 
     @Test
     fun `onMetrics should not emit non-observable metrics`() {
-        Thread.sleep(1000)
+        Thread.sleep(1100)
 
         clientMetricsCollector.addObserver(mockAudioVideoObserver)
         val rawMetrics = mutableMapOf(AudioClient.AUDIO_CLIENT_METRIC_MIC_DEVICE_FRAMES_LOST_PERCENT to 1.0)
@@ -58,7 +58,7 @@ class DefaultClientMetricsCollectorTest {
 
     @Test
     fun `onMetrics should not emit invalid metrics`() {
-        Thread.sleep(1000)
+        Thread.sleep(1100)
 
         clientMetricsCollector.addObserver(mockAudioVideoObserver)
         val rawMetrics = mutableMapOf(999 to 1.0)
