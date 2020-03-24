9b4af298c3c4b78290e25d2c0691339fd2a67457
Sang Lin <55719628+linsang21@users.noreply.github.com>
2020-03-23 19:38:38 -0700

    Update copyright

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/audio/activespeakerpolicy/ActiveSpeakerPolicy.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/audio/activespeakerpolicy/ActiveSpeakerPolicy.kt
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 package com.amazonaws.services.chime.sdk.meetings.audiovideo.audio.activespeakerpolicy
 
