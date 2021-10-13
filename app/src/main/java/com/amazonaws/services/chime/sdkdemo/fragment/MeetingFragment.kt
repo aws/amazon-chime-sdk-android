752724853180ad66eb487d0199fc654805b38a54
Chitesh Tewani <ctewani@amazon.com>
2021-10-12 21:30:07 -0700

    Remove unused import

--- app/src/main/java/com/amazonaws/services/chime/sdkdemo/fragment/MeetingFragment.kt
+++ app/src/main/java/com/amazonaws/services/chime/sdkdemo/fragment/MeetingFragment.kt
@@ -106,7 +106,6 @@ import com.google.android.material.tabs.TabLayout
 import com.google.gson.Gson
 import java.net.URL
 import java.util.Calendar
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
