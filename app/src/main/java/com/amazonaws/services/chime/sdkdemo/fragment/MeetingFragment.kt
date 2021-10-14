40fb108a9ee19f85c0424f639c25ec87d60a95b3
Chitesh Tewani <ctewani@amazon.com>
2021-10-14 00:19:38 -0700

    Add missing import

--- app/src/main/java/com/amazonaws/services/chime/sdkdemo/fragment/MeetingFragment.kt
+++ app/src/main/java/com/amazonaws/services/chime/sdkdemo/fragment/MeetingFragment.kt
@@ -14,6 +14,7 @@ import android.content.ServiceConnection
 import android.content.pm.PackageManager
 import android.media.projection.MediaProjectionManager
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
 import android.os.PowerManager
