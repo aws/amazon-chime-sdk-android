d68f63ec6e277645a3fca2d8e43948bddc7f904e
Henry Smith <hensmi@amazon.com>
2022-02-17 12:36:16 -0800

    Fix for comments

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/video/RemoteVideoSource.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/video/RemoteVideoSource.kt
@@ -1,15 +1,11 @@
 package com.amazonaws.services.chime.sdk.meetings.audiovideo.video
 
-class RemoteVideoSource(val attendeeId: String) {
-    override fun equals(o: Any?): Boolean {
-        if (o == this) {
-            return true
-        }
-
-        if (o !is RemoteVideoSource) {
-            return false
-        }
-
-        return this.attendeeId == o.attendeeId
-    }
-}
+/*
+ * A video source available in the current meeting. These need to be consistent between `remoteVideoSourcesDidBecomeAvailable`
+ * and `updateVideoSourceSubscriptions` as they are used as keys in maps that may be updated. I.e. when setting up
+ * a map for `updateVideoSourceSubscriptions` do not construct these yourselves or the configuration may or may not
+ * be updated.
+ *
+ * @property attendeeId: [String] - Attendee ID for the given source
+ */
+class RemoteVideoSource(val attendeeId: String)
--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/video/VideoSubscriptionConfiguration.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/video/VideoSubscriptionConfiguration.kt
@@ -1,7 +1,7 @@
 package com.amazonaws.services.chime.sdk.meetings.audiovideo.video
 
 /*
- * Remote video source initialized with attendeeId and hashed by object address. The values are intentionally mutable
+ * Configuration for a specific video configuration. The values are intentionally mutable
  * so that a map of all current configurations can be kept and updated as needed.
  *
  * @property priority: [VideoPriority] - Relative priority for the subscription. See [VideoPriority] for more information.
