1830e0799343a6e94461c867e4439e937ee77f22
Zhemin Tu <zhemitu@amazon.com>
2022-02-28 21:52:18 -0600

    Added NoDevice support

--- amazon-chime-sdk/src/test/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/audio/AudioModeTest.kt
+++ amazon-chime-sdk/src/test/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/audio/AudioModeTest.kt
@@ -9,6 +9,7 @@ class AudioModeTest {
         Assert.assertEquals(AudioMode.from(1), AudioMode.Mono16K)
         Assert.assertEquals(AudioMode.from(2), AudioMode.Mono48K)
         Assert.assertEquals(AudioMode.from(3), AudioMode.Stereo48K)
+        Assert.assertEquals(AudioMode.from(4), AudioMode.NoDevice)
     }
 
     @Test
