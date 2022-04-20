c436118a501b75adc4d57f850d5be63ded5630c7
Dingyi Shen <dingyshe@amazon.com>
2022-04-19 19:10:35 -0700

    test PR

--- amazon-chime-sdk/src/test/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/audio/activespeakerdetector/DefaultActiveSpeakerDetectorTest.kt
+++ amazon-chime-sdk/src/test/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/audio/activespeakerdetector/DefaultActiveSpeakerDetectorTest.kt
@@ -140,7 +140,7 @@ class DefaultActiveSpeakerDetectorTest {
             activeSpeakerObserverWithScore1
         )
         activeSpeakerDetector.onAttendeesJoined(arrayOf(testAttendeeInfo1))
-        Thread.sleep(2000)
+        Thread.sleep(3000)
         activeSpeakerDetector.removeActiveSpeakerObserver(activeSpeakerObserverWithScore1)
 
         verify(exactly = 2) {
@@ -161,7 +161,7 @@ class DefaultActiveSpeakerDetectorTest {
             activeSpeakerObserverWithScore2
         )
         activeSpeakerDetector.onAttendeesJoined(arrayOf(testAttendeeInfo1))
-        Thread.sleep(2000)
+        Thread.sleep(3000)
         activeSpeakerDetector.removeActiveSpeakerObserver(activeSpeakerObserverWithScore1)
         activeSpeakerDetector.removeActiveSpeakerObserver(activeSpeakerObserverWithScore2)
 
