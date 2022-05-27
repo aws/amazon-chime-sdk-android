f0ac36017490d736198ca3d2e1d7761972ac5d54
Nick Hwang <hokyungh@amazon.com>
2022-05-27 12:43:57 -0700

    Add flag

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/video/DefaultVideoClientController.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/video/DefaultVideoClientController.kt
@@ -50,6 +50,7 @@ class DefaultVideoClientController(
     private val TOPIC_REGEX = "^[a-zA-Z0-9_-]{1,36}$".toRegex()
     private val TAG = "DefaultVideoClientController"
 
+    private val VIDEO_CLIENT_FLAG_ENABLE_UNIFIED_PLAN = 1 shl 3
     private val VIDEO_CLIENT_FLAG_ENABLE_SEND_SIDE_BWE = 1 shl 5
     private val VIDEO_CLIENT_FLAG_ENABLE_USE_HW_DECODE_AND_RENDER = 1 shl 6
     private val VIDEO_CLIENT_FLAG_ENABLE_TWO_SIMULCAST_STREAMS = 1 shl 12
@@ -57,6 +58,7 @@ class DefaultVideoClientController(
     private val VIDEO_CLIENT_FLAG_DISABLE_CAPTURER = 1 shl 20
     private val VIDEO_CLIENT_FLAG_EXCLUDE_SELF_CONTENT_IN_INDEX = 1 shl 24
     private val VIDEO_CLIENT_FLAG_ENABLE_INBAND_TURN_CREDS = 1 shl 26
+    private val VIDEO_CLIENT_FLAG_ENABLE_NEW_SIGNALING_CLIENT = 1 shl 28
 
     private val gson = Gson()
 
@@ -254,6 +256,7 @@ class DefaultVideoClientController(
         logger.info(TAG, "Starting video client")
         videoClient?.setReceiving(false)
         var flag = 0
+        flag = flag or VIDEO_CLIENT_FLAG_ENABLE_UNIFIED_PLAN
         flag = flag or VIDEO_CLIENT_FLAG_ENABLE_SEND_SIDE_BWE
         flag = flag or VIDEO_CLIENT_FLAG_ENABLE_USE_HW_DECODE_AND_RENDER
         flag = flag or VIDEO_CLIENT_FLAG_ENABLE_TWO_SIMULCAST_STREAMS
@@ -261,6 +264,8 @@ class DefaultVideoClientController(
         flag = flag or VIDEO_CLIENT_FLAG_DISABLE_CAPTURER
         flag = flag or VIDEO_CLIENT_FLAG_EXCLUDE_SELF_CONTENT_IN_INDEX
         flag = flag or VIDEO_CLIENT_FLAG_ENABLE_INBAND_TURN_CREDS
+        flag = flag or VIDEO_CLIENT_FLAG_ENABLE_NEW_SIGNALING_CLIENT
+
         val videoClientConfig: VideoClientConfig = VideoClientConfigBuilder()
             .setMeetingId(configuration.meetingId)
             .setToken(configuration.credentials.joinToken)
