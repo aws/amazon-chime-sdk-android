e0ce4f59403f8cf19fae085f3af88f53da9ac736
Ethan Drake <69392035+ethanjanadrake@users.noreply.github.com>
2022-09-01 12:15:16 -0700

    Update AudioVideoObserver.kt

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/AudioVideoObserver.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/AudioVideoObserver.kt
@@ -125,9 +125,11 @@ interface AudioVideoObserver {
     fun onRemoteVideoSourceAvailable(sources: List<RemoteVideoSource>)
 
     /**
-     * Called when video capacity status is added or removed.
+     * Called when video capacity status is updated.
      *
      * Note: this callback will be called on main thread.
+     *
+     * @param available: Boolean - True if camera send is available (video capacity status is false).
      */
     fun onCameraSendAvailabilityUpdated(available: Boolean)
 }
