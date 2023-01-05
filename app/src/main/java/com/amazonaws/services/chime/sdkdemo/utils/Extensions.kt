288dd5e2a29b195fc6a4bf81c98e91bd00b54e08
George Zhang <100244039+georgezy-amzn@users.noreply.github.com>
2023-01-04 16:25:40 -0800

    Fixed the remote video render delay (#528)
    * Fixed the remote video/content share render delay

--- app/src/main/java/com/amazonaws/services/chime/sdkdemo/utils/Extensions.kt
+++ app/src/main/java/com/amazonaws/services/chime/sdkdemo/utils/Extensions.kt
@@ -13,6 +13,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import com.amazonaws.services.chime.sdk.meetings.audiovideo.video.RemoteVideoSource
+import com.amazonaws.services.chime.sdk.meetings.utils.DefaultModality
+import com.amazonaws.services.chime.sdk.meetings.utils.ModalityType
 import java.net.URLEncoder
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -35,4 +38,12 @@ fun isLandscapeMode(context: Context?): Boolean {
 
 fun formatTimestamp(timestamp: Long): String {
     return SimpleDateFormat("hh:mm:ss a").format(Date(timestamp))
+}
+
+fun String.isContentShare(): Boolean {
+    return DefaultModality(this).hasModality(ModalityType.Content)
+}
+
+fun RemoteVideoSource.isContentShare(): Boolean {
+    return this.attendeeId.isContentShare()
 }
