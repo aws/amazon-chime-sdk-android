dfb499c282ebe2cba43f2e7a611a2c4074179017
AMAN GUPTA <aguptaaa@amazon.com>
2023-04-04 14:12:45 -0700

    Add readable variable names

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/utils/TimezoneUtils.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/internal/utils/TimezoneUtils.kt
@@ -10,15 +10,18 @@ import java.util.TimeZone
 
 class TimezoneUtils {
     companion object {
+        private const val UTC_OFFSET_FORMAT = "%02d:%02d"
 
         /**
          * Return utc offset from timezone in +-hh:mm format.
          * E.g Asia/Calcutta timezone is returned as +05:30 UTC offset
          */
         fun getUtcOffset(timezone: TimeZone): String {
-            val offsetInMillis = timezone.getOffset(GregorianCalendar.getInstance(timezone).timeInMillis)
-            var offset: String = String.format("%02d:%02d", Math.abs(offsetInMillis / 3600000), Math.abs((offsetInMillis / 60000) % 60))
-            offset = (if (offsetInMillis >= 0) "+" else "-") + offset
+            val offsetMillis = timezone.getOffset(GregorianCalendar.getInstance(timezone).timeInMillis)
+            val offsetHours = Math.abs((offsetMillis / 60000) / 60)
+            val offsetMinutes = Math.abs((offsetMillis / 60000) % 60)
+            var offset: String = String.format(UTC_OFFSET_FORMAT, offsetHours, offsetMinutes)
+            offset = (if (offsetMillis >= 0) "+" else "-") + offset
             return offset
         }
     }
