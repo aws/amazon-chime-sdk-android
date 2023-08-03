267bf1946b7287933e3e98d9982891e5689dc68e
David Wang <davidwj@amazon.com>
2023-08-03 09:56:04 -0700

    Apply meeting constraints

--- amazon-chime-sdk/src/test/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/video/capture/ScreenCaptureResolutionCalculatorTest.kt
+++ amazon-chime-sdk/src/test/java/com/amazonaws/services/chime/sdk/meetings/audiovideo/video/capture/ScreenCaptureResolutionCalculatorTest.kt
@@ -71,6 +71,38 @@ class ScreenCaptureResolutionCalculatorTest {
     }
 
     @Test
+    fun `ScreenCaptureResolutionCalculator (weird number) should return scaled resolution when max(width, height) is above targetMaxVal and min(width, height) is above targetMinVal`() {
+        // compute targetWidth and targetHeight with alignment
+        val width: Int = 7139
+        val height: Int = 3217
+        val targetSize = screenCaptureResolutionCalculator.computeTargetSize(width, height)
+        val alignedWidth: Int = targetSize[0]
+        val alignedHeight: Int = targetSize[1]
+        val maxVal: Int = max(alignedWidth, alignedHeight)
+        val minVal: Int = min(alignedWidth, alignedHeight)
+        val originalAspectRatio: Int = (1000 * width.toDouble() / height.toDouble()).toInt()
+        val scaledAspectRatio: Int = (1000 * alignedWidth.toDouble() / alignedHeight.toDouble()).toInt()
+        val aspectRatioDiff: Int = abs(scaledAspectRatio - originalAspectRatio)
+        assert(minVal <= targetMinVal && maxVal <= targetMaxVal && aspectRatioDiff < aspectRatioDiffThreshold)
+    }
+
+    @Test
+    fun `ScreenCaptureResolutionCalculator (extra large number) should return scaled resolution when max(width, height) is above targetMaxVal and min(width, height) is above targetMinVal`() {
+        // compute targetWidth and targetHeight with alignment
+        val width: Int =  93215327
+        val height: Int = 32171121
+        val targetSize = screenCaptureResolutionCalculator.computeTargetSize(width, height)
+        val alignedWidth: Int = targetSize[0]
+        val alignedHeight: Int = targetSize[1]
+        val maxVal: Int = max(alignedWidth, alignedHeight)
+        val minVal: Int = min(alignedWidth, alignedHeight)
+        val originalAspectRatio: Int = (1000 * width.toDouble() / height.toDouble()).toInt()
+        val scaledAspectRatio: Int = (1000 * alignedWidth.toDouble() / alignedHeight.toDouble()).toInt()
+        val aspectRatioDiff: Int = abs(scaledAspectRatio - originalAspectRatio)
+        assert(minVal <= targetMinVal && maxVal <= targetMaxVal && aspectRatioDiff < aspectRatioDiffThreshold)
+    }
+
+    @Test
     fun `ScreenCaptureResolutionCalculator (landscape) should return scaled resolution when max(width, height) is above targetMaxVal and min(width, height) is above targetMinVal`() {
         // compute targetWidth and targetHeight with alignment
         val width: Int = 3840
