7feb409652970bfbebd3d6b6d79d5a08cf8a7b28
George Zhang <georgezy@amazon.com>
2024-05-03 16:36:02 -0700

    Update MeetingSessionStatusCode comment

--- amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/session/MeetingSessionStatusCode.kt
+++ amazon-chime-sdk/src/main/java/com/amazonaws/services/chime/sdk/meetings/session/MeetingSessionStatusCode.kt
@@ -32,32 +32,44 @@ enum class MeetingSessionStatusCode(val value: Int) {
     AudioDisconnectAudio(3),
 
     /**
-     * Authentication was rejected. The client is not allowed on this call.
+     * Authentication was rejected as the attendee information in MeetingSessionCredentials did not
+     * match that of an attendee created via chime::CreateAttendee.
+     * This error may imply an issue with your credential providing service,
+     * the client will not be allowed on this call.
      */
     AudioAuthenticationRejected(4),
 
     /**
-     * The client can not join because the call is at capacity.
+     * The client can not join because the meeting is at capacity.
+     * The service supports up to 250 attendees.
      */
     AudioCallAtCapacity(5),
 
     /**
-     * The call was ended.
+     * The attendee attempted to join a meeting that has already ended.
+     * See this [FAQ](https://aws.github.io/amazon-chime-sdk-js/modules/faqs.html#when-does-an-amazon-chime-sdk-meeting-end)
+     * for more information. The end user may want to be notified of this type of error.
      */
     AudioCallEnded(6),
 
     /**
-     * There was an internal server error with the audio leg.
+     * There was an internal server error related to audio.
+     * This may indicate some issue with the audio device,
+     * or an issue with the Amazon Chime SDK service itself
      */
     AudioInternalServerError(7),
 
     /**
-     * Could not connect the audio leg due to the service being unavailable.
+     * There was an internal server error related to audio.
+     * This may indicate some issue with the audio device,
+     * or an issue with the Amazon Chime SDK service itself.
      */
     AudioServiceUnavailable(8),
 
     /**
-     * The audio leg failed.
+     * There was an internal server error related to audio.
+     * This may indicate some issue with the audio device,
+     * or an issue with the Amazon Chime SDK service itself.
      */
     AudioDisconnected(9),
 
@@ -67,12 +79,16 @@ enum class MeetingSessionStatusCode(val value: Int) {
     ConnectionHealthReconnect(10),
 
     /**
-     * The network has become poor and is no longer good enough for VoIP.
+     * Network is not good enough for VoIP, `AudioVideoObserver.audioSessionDidDrop()` will be
+     * triggered, and there will an automatic attempt of reconnecting.
+     * If the reconnecting is successful, `onAudioSessionStarted` will be called with value of
+     * reconnecting as true.
      */
     NetworkBecamePoor(11),
 
     /**
-     * Video Client Failed.
+     * There was an internal server error related to video. This may indicate some issue with the
+     * video device, or an issue with the Amazon Chime SDK service itself.
      */
     VideoServiceFailed(12),
 
